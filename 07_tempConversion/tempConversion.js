3b80735b1344d3a28796a42d2db62c0a554d1c10
bulbinator <smmaisum@gmail.com>
2025-01-03 16:25:18 -0600

    passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(t)
+{
+  t = (t-32) * (5/9)
+  return parseFloat(t.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(t)
+{
+  t = t * (9/5) + 32
+  return parseFloat(t.toFixed(1))
 };
 
 // Do not edit below this line
