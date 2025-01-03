27aa038a171ec29b86b73c861cf0df35e00300c0
bulbinator <smmaisum@gmail.com>
2025-01-03 16:07:15 -0600

    passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) return "ERROR"
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR"
+    if (x > y)
+    {
+        let temp = x
+        x = y
+        y = temp
+    }
+    
+    let sum = 0
+    for (let i=x; i<=y; i++)
+    {
+        sum = sum + i
+    }
+
+    return sum
 
 };
 
