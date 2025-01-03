45233cb50309a5c1eca5dc55d9f42a7312dc605b
bulbinator <smmaisum@gmail.com>
2025-01-03 14:31:04 -0600

    passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    s = s.split("")
+    let r = ""
+    for (let i=s.length-1; i>=0; i--)
+    {
+        r = r + s[i]
+    }
+    return r
 };
 
 // Do not edit below this line
